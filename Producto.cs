b9cb610691dcfa29a833468bf913b9ab27e5d06b
Fernabim <109820905+FernandaNav@users.noreply.github.com>
2024-07-31 21:51:48 -0600

    agregada la estructura del switch y se ha instanciado la clase producto.

--- Producto.cs
+++ Producto.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace INVENTARIODETIENDA
 {
-    internal class Producto
+    public class Producto
     {
+        public string NombreProducto { get; set; }
+        public double PrecioProducto { get; set; }
+        public int CantidadEnStock { get; set; }
+
+        public Producto(string nombreProducto, double precioProducto, int cantidadEnStock)
+        {
+            NombreProducto = nombreProducto;
+            PrecioProducto = precioProducto;
+            CantidadEnStock = cantidadEnStock;
+        }
+
+        public string NombredDelProducto()
+        {
+            string nombre;
+            Console.Write("Ingresa el nombre del primer producto: ");
+            nombre = Console.ReadLine();
+            return nombre;
+        }
+        public void MensajeDeError()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Error de formato");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("Pulsa cualquier tecla para continuar...");
+            Console.ResetColor(); Console.ReadKey(); Console.Clear();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,61 @@
-﻿static void Main(string[] args)
+﻿using INVENTARIODETIENDA;
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+class Program
 {
-    Console.WriteLine("-------------------------");
-    Console.WriteLine("        TIENDA");
-    Console.WriteLine("-------------------------");
+    static void Main(string[] args)
+    {
+        int opcion = 0;
+        string nombreProducto ="";
+        double precioProducto=0;
+        int cantidadEnStock=0;
+        Producto producto = new Producto(nombreProducto, precioProducto, cantidadEnStock);
+        do
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("-------------------------"); Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("        TIENDA"); Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("-------------------------"); Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("");
+            Console.WriteLine("Opción 1: Agregar producto.");
+            Console.WriteLine("Opción 2: Mostrar datos.");
+            Console.WriteLine("Opción 3: Vender producto.");
+            Console.WriteLine("Opción 4. Reabastecer producto.");
+            Console.WriteLine("Opción 5: Actualizar precio.");
+            Console.WriteLine("Opción 6: Agregar nuevo producto.");
+            Console.WriteLine("Opción 7: Salir.");
+            Console.WriteLine(); Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("Elija la opción que desea: ");
+            try
+            {
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Clear();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    default:
+                        break;
+                }
+
+            }catch(FormatException)
+            {
+                producto.MensajeDeError();
+            }
+            Console.ResetColor();
+        } while (opcion != 7);
+    }
 }
